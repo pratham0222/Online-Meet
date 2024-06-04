2f96b20e034fbe75ae56834f9def82093a8a6188
pratham0222 <prathamvj02@gmail.com>
2024-06-04 22:01:32 +0530

    navbar, sidebar, basic layout

---
+++ constants/index.ts
@@ -0,0 +1,27 @@
+export const sidebarLinks = [
+  {
+    label: "Home",
+    route: "/",
+    imageUrl: "/icons/Home.svg",
+  },
+  {
+    label: "UpComing",
+    route: "/upcoming",
+    imageUrl: "/icons/upcoming.svg",
+  },
+  {
+    label: "Previous",
+    route: "/previous",
+    imageUrl: "/icons/previous.svg",
+  },
+  {
+    label: "Recording",
+    route: "/recordings",
+    imageUrl: "/icons/Video.svg",
+  },
+  {
+    label: "Personal Room",
+    route: "/personal-room",
+    imageUrl: "/icons/add-personal.svg",
+  },
+];
